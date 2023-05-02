c78db73d7d101d9551cf598fd9ac7cb3c1baaf30
Lucas-Huerta <lucas.huerta@edu.univ-fcomte.fr>
2023-05-02 22:39:48 +0200

    [EDIT] footer && contact section

--- src/App.vue
+++ src/App.vue
@@ -29,16 +29,74 @@
       </div>
     </section>
     <main class="main">
-      <section class="wrapper-project">
-        <Project v-for="project in tabProject" :key="project" :projet="project"/>
+      <div class="bg">
+        <section class="wrapper-project">
+          <Project v-for="project in tabProject" :key="project" :projet="project"/>
+        </section>
+        <section class="wrapper-competences">
+          <div data-scroll data-scroll-direction="horizontal" data-scroll-speed="6" class="high">
+            <span v-for="comp in tabHighComp" :key="comp">{{ comp }}</span>
+          </div>
+          <div data-scroll data-scroll-direction="horizontal" data-scroll-speed="-6" class="bottom">
+            <span v-for="comp in tabBottomComp" :key="comp">{{ comp }}</span>
+          </div>
+        </section>
+      </div>
+      <section class="wrapper-infos">
+        <h3>
+          About
+        </h3>
+        <div class="container-infos">
+          <div class="row-infos">
+            <p>Lucas</p> <p>Huerta</p>
+          </div>
+          <div class="row-infos">
+            <span>21y</span>
+            <p>
+              Paris
+            </p>
+          </div>
+          <div class="row-infos telNumber" ref="telNumber">
+            <span>+</span><span>3</span><span>3</span><span>7</span><span>8</span><span>3</span><span>8</span><span>0</span><span>6</span><span>3</span><span>5</span><span>7</span>
+          </div>
+          <div class="row-infos">
+            <p>Spotify</p> <p>Playlist</p>
+          </div>
+        </div>
       </section>
-      <section class="wrapper-competences">
-        <div data-scroll data-scroll-direction="horizontal" data-scroll-speed="6" class="high">
-          <span v-for="comp in tabHighComp" :key="comp">{{ comp }}</span>
-        </div>
-        <div data-scroll data-scroll-direction="horizontal" data-scroll-speed="-6" class="bottom">
-          <span v-for="comp in tabBottomComp" :key="comp">{{ comp }}</span>
-        </div>
+      <section class="footer-section">
+        <footer>
+          <div class="container-hello">
+            <p class="hello">
+              Hello
+            </p>
+            <p class="feel">
+              Feel free to send me a message !
+            </p>
+          </div>
+          <div class="bottom-section">
+            <div class="container-contact">
+              <p>Contact me</p>
+            </div>
+            <div class="container-bottom">
+              <div class="date">
+                <p>
+                  Local time
+                </p>
+                <span>{{ formattedDay }}</span>
+                <span> {{ formattedDate }}</span>
+              </div>
+                <p>
+                  Lucas huerta 
+                </p>
+              <div class="rs">
+                <span><a href="https://www.linkedin.com/in/lucas-huerta13/" target="_blank">Linkedin</a></span>
+                <span><a href="https://github.com/Lucas-Huerta" target="_blank">GitHub</a></span>
+                <span><a href="" target="_blank">CV</a></span>
+              </div>
+            </div>
+          </div>
+        </footer>
       </section>
     </main>
   </div>
@@ -48,10 +106,10 @@
 import Navbar from './components/Navbar.vue';
 import Project from './components/Project.vue';
 import LocomotiveScroll from 'locomotive-scroll';
-import { ref, onMounted } from 'vue';
+import { ref, onMounted, onUpdated } from 'vue';
 
 const wrapper = ref();
-// const scrollY = ref(0);
+const scrollY = ref(0);
 let imgStar = ref();
 
 let tabProject = ref([
@@ -84,27 +142,49 @@ let tabProject = ref([
 
 let tabHighComp = ref(["Fullstack developper", "Digital developper", "Internet chil", "Fullstack developper", "Digital developper", "Internet chil", "Fullstack developper", "Digital developper", "Internet chil"])
 let tabBottomComp = ref(["Vue Js", "Nuxt Js", "Node Js", "CI/CD", "Vue Js", "Nuxt Js", "Node Js", "CI/CD", "Vue Js", "Nuxt Js", "Node Js", "CI/CD"])
+let formattedDate = ref();
+let formattedDay = ref();
 
-// const handleScroll = async() => {
-//   scrollY.value = window.scrollY;
-//   imgStar.value.style.transform = `rotate(${scrollY.value/5}deg)`;
-//   imgStar.value.style.transform = 'all linear';
-//   imgStar.value.style.position = 'fixed';
-// }
+const handleScroll = async() => {
+  scrollY.value = window.scrollY;
+  imgStar.value.style.transform = `rotate(${scrollY.value/5}deg)`;
+  imgStar.value.style.transform = 'all linear';
+  imgStar.value.style.position = 'fixed';
+}
 
 const locomotive = async() => {
   await new LocomotiveScroll({
-    // el: wrapper.value,
-    el: document.querySelector('[data-scroll-container]'),
+    el: wrapper.value,
+    // el: document.querySelector('[data-scroll-container]'),
     // el: document.getElementById('app'),
     smooth: true, 
   });
 }
 
+const haveLocalTime = async() =>{
+  const now = new Date();
+  const formatter = new Intl.DateTimeFormat('fr', { 
+    hour: 'numeric', 
+    minute: 'numeric', 
+    second: 'numeric' 
+  });
+
+  const formatterDay = new Intl.DateTimeFormat('en', { 
+    weekday: 'long',
+    year: 'numeric',
+    month: 'long',
+    day: 'numeric', 
+  });
+
+  formattedDay.value = formatterDay.format(now);
+  formattedDate.value = formatter.format(now);
+}
+
 onMounted(async() => {
   // await handleScroll()
-  // window.addEventListener('scroll', handleScroll);
+  window.addEventListener('scroll', handleScroll);
   await locomotive();
+  setInterval(haveLocalTime, 1000);
 })
 
 </script>
@@ -196,8 +276,13 @@ onMounted(async() => {
 
 .main{
   height: max-content;
+  width: 100%;
+}
+
+.bg{
   background-color: var(--black-color);
   width: 100%;
+  height: 100%;
 }
 
 .wrapper-project{
@@ -213,7 +298,6 @@ onMounted(async() => {
   display: flex;
   flex-direction: column;
   width: 100%;
-  padding-bottom: 50vh;
   color: white;
 }
 
@@ -235,5 +319,151 @@ onMounted(async() => {
 .wrapper-competences span{
   font-family: "Dahlia";
   font-size: 90px;
+}
+
+.wrapper-infos{
+  width: 100%;
+  height: max-content;
+  margin-bottom: 10vh;
+}
+
+.wrapper-infos h3{
+ font-family: "Dahlia";
+ text-align: center;
+ font-size: 60px;
+}
+
+.container-infos{
+  width: 60%;
+  height: 100%;
+  margin: auto;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  font-size: 60px;
+  font-weight: 600;
+  font-family: "Inter", sans-serif;
+}
+
+.row-infos{
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+}
+.row-infos p{
+  margin: 0;
+}
+.telNumber:hover span:nth-child(even){
+  transition: all 0.2s linear;
+  transform: translate3d(-3px, -20px, 0);
+  rotate: 10deg;
+}
+
+.telNumber:hover span:nth-child(odd){
+  transition: all 0.2s linear;
+  transform: translate3d(3px, 20px, 0);
+  rotate: -10deg;
+}
+
+footer{
+  width: 100%;
+  height: 100%;
+}
+
+.footer-section{
+  width: 100%;
+  color: white;
+  height: 100vh;
+  background-color: var(--black-color);
+}
+
+.container-hello{
+  width: 100%;
+  text-align: center;
+  display: flex;
+  flex-direction: column;
+  position: relative;
+}
+
+.bottom-section{
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  position: absolute;
+  bottom: 0;
+}
+
+.container-bottom{
+  width: 100%;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: end;
+  margin-right: 5vw;
+}
+
+.container-bottom p{
+  margin: 0;
+}
+
+.hello{
+  font-size: 430px;
+  font-family: 'Inter';
+  font-weight: bold;
+  text-transform:uppercase;
+  margin: 0;
+  position: absolute;
+  transform: translate(-50%);
+  left: 50%;
+  opacity: 0.2;
+}
+
+.feel{
+  position: absolute;
+  transform: translate(-50%);
+  left: 50%;
+  top: 35vh;
+  color: var(--active-color);
+  font-weight: bold;
+  font-size: 50px;
+  width: 100%;
+}
+
+.container-contact{
+  padding: 1vh 3vw;
+  background-color: var(--active-color);
+  border-radius: 12px;
+  text-transform: uppercase;
+  margin-bottom: 10vh;
+}
+.rs{
+  display: flex;
+  flex-direction: row;
+  justify-content: flex-end;
+  width: 10%;
+  gap: 2vw;
+}
+
+.rs a{
+  text-decoration: none;
+  color: white;
+}
+
+.container-contact p{
+  margin: 0;
+}
+
+.date{
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  margin-left: 5vw;
+}
+
+.date p{
+  margin: 0;
+  font-weight: bold;
+  font-size: 20px;
 }
 </style>
